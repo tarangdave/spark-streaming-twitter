c46124b3ee3cef6affe09afcf019ee39217e14b7
Tarang Dave <tarangadave@gmail.com>
2018-11-17 00:03:20 -0800

    add streaming reduce by window function

---
+++ streamingreducebywindow.py
@@ -0,0 +1,20 @@
+import sys
+
+from pyspark import SparkContext
+from pyspark.streaming import StreamingContext
+
+if __name__ == "__main__":
+    sc = SparkContext(appName="StreamingWindowSum")
+    ssc = StreamingContext(sc, 2)
+
+    ssc.checkpoint("/tmp/spark")
+
+    lines = ssc.socketTextStream(sys.argv[1], int(sys.argv[2]))
+    sum = lines.reduceByWindow(
+        lambda x,y:int(x) + int(y),
+        lambda x,y: int(x) - int(y),
+        10,2)
+    sum.pprint()
+
+    ssc.start()
+    ssc.awaitTermination()
